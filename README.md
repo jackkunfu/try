40642d34970a4d9d48d3069919328e891a7dd3bd
shanguoqiang <guoqiang.shan@healthbok.com>
2017-08-15 17:57:01 +0800

    zhegnli

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 ### 平时的各个小demo
-* vue demos
-    - 多重循环嵌套渲染
-    - 数据之间的更改，联动
-* 图片hover 变换角度demo
-* delayTrigger.js
-	- 限制多次点击时的多次触发操作
-* ant-design
-	- 目前只是官方脚手架工具生成的demo
+* 一些尝试
---
+++ projects/api-sku/src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    .hello
+        | 1111
+        span wewe
+</template>
+   
+
+<script>
+export default {
+    name: 'hello',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style lang="less">
+</style>
+    
