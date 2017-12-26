6ac4c2abe363e128b5f2afed61361b81f473246a
shanguoqiang <guoqiang.shan@healthbok.com>
2017-12-26 18:47:36 +0800

    gx

--- projects/ionicTry/src/pages/hello-ionic/hello-ionic.ts
+++ projects/ionicTry/src/pages/hello-ionic/hello-ionic.ts
@@ -5,15 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  list: any[];
+
   constructor() {
+
+    // 爬取百度图片首页最热列表
+    this.list = [];
+    var url = encodeURIComponent('http://image.baidu.com/search/acjson?tn=resultjson_com&catename=pcindexhot&ipn=rj&ct=201326592&is=&fp=result&queryWord=&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=pcindexhot&face=0&istype=2&qc=&nc=1&fr=&pn=0&rn=30')
+    this.paQuUrl(url)
+  }
+
+  // 爬取地址图片数据
+  paQuUrl(url) {
     var xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://localhost:3000/paqu?url=http://image.baidu.com/search/acjson?tn=resultjson_com&catename=pcindexhot&ipn=rj&ct=201326592&is=&fp=result&queryWord=&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=pcindexhot&face=0&istype=2&qc=&nc=1&fr=&pn=0&rn=30', false)
-    xhr.onreadystatechange=function(){
-      if(xhr.status == 4 && xhr.readyState == 200){
-        console.log(xhr.responseText);
+    xhr.open('get', 'http://localhost:3000/paqu?url='+url, true)
+    xhr.onreadystatechange = ()=>{
+      if(xhr.status == 200 && xhr.readyState == 4){
+        var data = JSON.parse(xhr.responseText)
+        if(data.success) this.list = data.data;
       }
     }
-    // xhr.send('url=http://image.baidu.com/search/acjson?tn=resultjson_com&catename=pcindexhot&ipn=rj&ct=201326592&is=&fp=result&queryWord=&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=pcindexhot&face=0&istype=2&qc=&nc=1&fr=&pn=0&rn=30')
     xhr.send(null)
   }
 }
