9457b1f8d58db994047064088a7f9b2201cde46d
shanguoqiang <guoqiang.shan@healthbok.com>
2017-12-27 21:56:20 +0800

    gx

--- features/管理页面增删改查/function.js
+++ features/管理页面增删改查/function.js
@@ -10,6 +10,7 @@ function handleOptions(options) {
         inputs._sy = inputs._sy || _sy;
         inputs._t = inputs._t || new Date().getTime();
         inputs._v = inputs._v || '1.0';
+        inputs._se = localStorage._se || '';
         return inputs;
     }
     var buildAt = function(inputs) {
@@ -37,10 +38,19 @@ function handleOptions(options) {
     return op
 }
 
-function ajax(type,url,opts,cb){
+function ajax(opts,cb){
+    var url;
+    var currentDomain = window.location.host;
+    if(currentDomain == "h5.stg.iputong.com") {
+        url = "http://120.26.91.195:8080/mapi";
+    } else if (currentDomain == "h5.iputong.com") {
+        url = "http://api.iputong.com/mapi";
+    } else {
+        url = "http://172.16.8.194/mapi";
+    }
     $.ajax({
         url: url,
-        type: type,
+        type: 'get',
         dataType: 'jsonp',
         crossDomain: true,
         xhrFields: {
@@ -48,7 +58,8 @@ function ajax(type,url,opts,cb){
         },
         data: handleOptions(opts)
     }).done(function(data){
-        cb(data);
+        if(!data.success) alert(data.msgInfo)
+        data.success && cb(data);
     }).fail(function(e){
         alert(e)
     })
