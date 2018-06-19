4f9af4201ef36e3917083b378395d6ef3f85cb66
wadaa <632792338@qq.com>
2018-06-20 00:27:48 +0800

    gx

--- wb/mobile/src/pages/zwgzh/pay.vue
+++ wb/mobile/src/pages/zwgzh/pay.vue
@@ -1,89 +1,63 @@
 <template lang="pug">
 .h100
-    .bg
-        img(src="../../assets/apply_bg@2x.png")
+    div
+        .item
+            span(style="font-weight:bold") 订单名称：{{order.name}}
+        .item
+            span 订单金额：{{order.fee}}
+        .item
+            span 地区：{{order.city}}
+        .item
+            span 训练营：{{order.train}}
+        .item
+            span 卡种：{{order.cardType}}
+        .item
+            span 卡种：{{order.times}}
 
-    // .top
-        img(src="../../assets/activity_logo@2x.png")
+    div
+        .item(@click="payWay=1")
+            img(src="../../assets/wx.png")
+            span 微信支付
+            .fr
+                img(src="../../assets/choose.png" v-if="payWay==1")
+                img(src="../../assets/nochoose.png" v-else)
 
-    .enroll
-        .each
-            span 选择地区
-            select(v-model="item.city" :class="item.phone==''?'':'ff'" placeholder="请输入手机号")
-                option 1
-            img(src="../../assets/xia.png")
+        .item(@click="payWay=2")
+            img(src="../../assets/zfb.png")
+            span 微信支付
+            .fr
+                img(src="../../assets/choose.png" v-if="payWay==2")
+                img(src="../../assets/nochoose.png" v-else)
 
-        .each
-            span 选择训练营
-            select(v-model="item.train" :class="item.phone==''?'':'ff'" placeholder="请输入手机号")
-                option 1
-            img(src="../../assets/xia.png")
-
-        .each
-            span 选择卡种
-            select(v-model="item.cardType" :class="item.phone==''?'':'ff'" placeholder="请输入手机号")
-                option 1
-            img(src="../../assets/xia.png")
-
-        .each
-            span 选择训练频次
-            select(v-model="item.times" :class="item.phone==''?'':'ff'" placeholder="请输入手机号")
-                option 1
-            img(src="../../assets/xia.png")
         
-        .each
-            span 接待课程顾问
-            select(v-model="item.people" :class="item.phone==''?'':'ff'" placeholder="请输入手机号")
-                option 1
-            img(src="../../assets/xia.png")
-
-        .tip 注意：付款完成后请根据您的卡种选择上课时间并完善学员信息
-
-        .money 费用合计：
-            span 2000
-            | 元
-        
-        .btn(@click="baoming") 支付报名
+    .btn(@click="pay") 确认支付
 
 </template>
 
 <script>
     export default {
-        name: 'Baoming',
+        name: 'pay',
         data () {
             var query = this.$route.query;
             return {
-                item: {
-                    city: '', train: '', cardType: '', times: '', people: ''
-                }
+                order: {
+                    name: '郑武体育篮球训练课', fee: '3000元', city: '杭州', train: '小龙训练营', cardType: '半年卡', times: '每周两次'
+                },
+                payWay: 0
             }
         },
         mounted(){
             
         },
         methods: {
-            banzhurenLogin(){
-                this.goUrl('/banzhuren')
-            },
-            goMy(){
-                this.goUrl('/my')
-            },
-            baoming(){
-                this.goUrl('/banzhuren')
-            },
-            async loginFun(){
-                var item = this.item;
-                if(item.city == '') return this.messageTip('地区未选~');
-                if(item.city == '') return this.messageTip('训练营未选~');
-                if(item.city == '') return this.messageTip('卡种未选~');
-                if(item.city == '') return this.messageTip('训练频次未选~');
-                if(item.city == '') return this.messageTip('课程顾问~');
+            pay(){
+                if(this.payWay == 1){
 
-                var res = await this.ajax('/api/user/login', this.item);
-                if(res && res.status == 200){
-                    var data = res.data;
-                    // this.goUrl('/myset', { tb_tk: data.token, tb_userInfo: JSON.stringify(data.tbUser) });
+                }else if(this.payWay == 2){
+
                 }
+                this.messageTip('支付成功')
+                this.goUrl('/my')
             }
         }
     }
@@ -92,98 +66,33 @@
 <style lang="sass" scoped>
 .h100
     background: #f3f3f3
-
-.bg
-    width: 100%
-    height: 100%
-    position: absolute
-    left: 0
-    top: 0
-    z-index: 0
-    img
-        width: 100%
-        height: auto
-
-.top
-    text-align: center
-    img
-        width: 70%
-        margin: 2rem auto
-
-.enroll
-    margin: 13rem auto 0
-    width: 16rem
-
-.tip
-    font-size: 0.6rem
-    color: #c6c6c6
-    padding: 0 0.5rem
-    margin-top: -0.5rem
-
-.money
-    text-align: center
-    font-size: 0.8rem
-    margin-top: 1rem
-    span
-        color: #48C4F5
-
-select
-    padding: 0 0.8rem
-    width: 100%
-    color: #9b9b9b
-    line-height: 1.4rem
-    background-size: 6%
-    position: relative
-    z-index: 1
-    background: none
-    &.ff
+    >div
         background: #fff
+        margin-bottom: 0.7rem
+        .item
+            height: 2.7rem
+            // line-height: 2.7rem
+            font-size: 0.8rem
+            border-bottom: 0.05rem solid #eee
+            margin-bottom: 0
+            padding: 0.8rem 1.4rem
+            color: #6f6f6f
 
-.each
-    // border-bottom: 1px solid #e3e3e3
-    padding: 0.4rem 0
-    margin-bottom: 0.9rem
-    position: relative
-    background: #fff
-    height: 2.1rem
-    > span
-        position: absolute
-        left: 0.8rem
-        top: 0
+            img
+                vertical-align: middle
+                width: 1rem
+                margin-right: 0.5rem
+                position: relative
+                top: -0.1rem
+
+    .btn
+        width: 16rem
+        margin: 1rem auto
         line-height: 2.1rem
-        color: #9b9b9b
-
-    img
-        position: absolute
-        z-index: 2
-        width: 0.9rem
-        top: 0.8rem
-        right: 0.8rem
-
-    input
-        border: none
-        color: #9b9b9b
-        line-height: 1rem
-        width: 6rem
-
-.login-tip
-    width: 15rem
-    margin: 0 auto
-    color: #48C4F5
-    img
-        width: 0.88rem
-        float: left
-        margin-right: 0.3rem
-
-.btn
-    width: 15rem
-    margin: 1rem auto
-    background: #48C4F5
-    line-height: 2.1rem
-    text-align: center
-    color: #fff
-    font-size: 0.8rem
-    border-radius: 0.4rem
-
+        background: #48C4F5
+        font-size: 0.8rem
+        text-align: center
+        color: #fff
+        border-radius: 0.2rem
 </style>
 
