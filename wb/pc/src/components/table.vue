5facaded14a055495065cb810b2774203fecf0a4
wadaa <632792338@qq.com>
2018-06-29 23:21:57 +0800

    gx

--- wb/pc/src/components/table.vue
+++ wb/pc/src/components/table.vue
@@ -29,7 +29,7 @@
                 //- 功能
                 el-table-column(:prop="item.key || ''" :label="item.str"  v-else-if="item.type == 'fun'")
                     template(slot-scope="scope")
-                        span(v-if="item.isShow && item.isShow.key" @click="$emit(item.fun, scope)" style="cursor:pointer;" :class="item.class ? item.class : ''") {{scope.row[item.key] || item.text}}
+                        span(@click="$emit(item.fun, scope)" style="cursor:pointer;" :class="item.class ? item.class : ''") {{scope.row[item.key] || item.text}}
 
                 //- 无需处理内容展示
                 el-table-column(:prop="item.key" :label="item.str" v-else="")
@@ -37,8 +37,9 @@
             //- 操作
             el-table-column(label="操作" v-if="scopeOperates && scopeOperates.length>0" ref="operate" width="150")
                 template(slot-scope="scope")
-                    el-button(v-for="(op, i) in scopeOperates" :type="op.type || 'success'" size="mini" :key="op.str"
-                        @click="$emit(op.fun, scope)" plain) {{op.str}}
+                    template(v-for="(op, i) in scopeOperates")
+                        el-button(v-if="!op.isShow || (op.isShow && op.isShow.key)" :type="op.type || 'success'" size="mini" :key="op.str"
+                            @click="$emit(op.fun, scope)" plain) {{op.str}}
 
         el-pagination(layout="total, prev, pager, next, jumper" :total="total" :page-size="limit" 
             @current-change="(v)=>{$emit('changePage', v)}" ref="page")
