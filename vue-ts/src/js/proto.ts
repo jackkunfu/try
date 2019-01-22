f72ce92b790257f991f81c51770cc5732d5cfa93
sgq <shanguoqiang@zailingtech.com>
2019-01-22 17:19:01 +0800

    gx

--- vue-ts/src/js/proto.ts
+++ vue-ts/src/js/proto.ts
@@ -1,13 +1,39 @@
-export default function (Vue) {
-  let _fetch =  function(url, parama, otherConfig) {
-    let req = await fetch({
-      method: 
-    })
+import { Component } from 'vue';
+import { Constructor } from 'vue/types/options';
+
+interface FetchParams {
+  url: string;
+  parama: any;
+  otherConfig: any;
+}
+
+async function ajax(...argus: any) {
+  // url: string, parama: object , type?: string, otherConfig?: any
+  let opts = {
+    method: argus[2] || 'post',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'token': ''
+    },
+    body: ''
   }
-  Vue.prototype.fetch = function () {
+  if (argus.otherConfig) {
+    opts = Object.assign(opts, {...argus.otherConfig})
+  }
+  if (process.env.NODE_ENV === 'development') {
+    opts.headers.token = '123456789'
+  }
+  if (argus[1]) {
+    opts.body = JSON.stringify(argus[1])
+  }
+  return fetch(argus[0], opts)
+}
+
+export default function(Vue: Constructor) {
+  Vue.prototype.fetch = async function(url: string, parama: object|string, type: string, config?: any) {
     try {
-      let options = { ...arguments }
-      let res = _fetch(options)
+      let res = await ajax(url, parama, type, config);
       if(res.ok){
         let result = config && config.cType === 'html' ? res.text() : res.json()
         if (result.code === this.successCode) {
@@ -20,7 +46,7 @@ export default function (Vue) {
           return null
         }
       } else {
-        return bull
+        return null
       } 
     } catch (e) {
       console.log(e)
